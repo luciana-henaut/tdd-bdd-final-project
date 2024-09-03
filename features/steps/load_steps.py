4238fec7841a88f5e320812b162f478e5c3d7681
Your Name <you@example.com>
2024-09-03 08:48:48 -0400

    changes

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -13,23 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ######################################################################
-
 """
 Product Steps
-
 Steps file for products.feature
-
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
 import requests
 from behave import given
-
 # HTTP Return Codes
 HTTP_200_OK = 200
 HTTP_201_CREATED = 201
 HTTP_204_NO_CONTENT = 204
-
 @given('the following products')
 def step_impl(context):
     """ Delete all Products and load new ones """
@@ -42,7 +37,6 @@ def step_impl(context):
     for product in context.resp.json():
         context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
         assert(context.resp.status_code == HTTP_204_NO_CONTENT)
-
     #
     # load the database with new products
     #
@@ -50,3 +44,11 @@ def step_impl(context):
         #
         # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
         #
+        context.resp = requests.post(rest_endpoint, json={
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": True if row['available'] == "True" else False,
+            "category": row['category'],
+        })
+        assert(context.resp.status_code == HTTP_201_CREATED)
